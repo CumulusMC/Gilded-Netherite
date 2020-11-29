9db224e8d5952d671bd43c20fd3968f21aac80da
Gareth Coles <gdude2002@gmail.com>
2020-11-29 22:06:28 +0000

    The enchantments weren't real anyway

--- src/main/java/com/cumulusmc/gildednetherite/items/RegisterItems.java
+++ src/main/java/com/cumulusmc/gildednetherite/items/RegisterItems.java
@@ -17,7 +17,7 @@ public class RegisterItems {
     public static final ArmorMaterial gildedNetheriteArmorMaterial = new GildedNetheriteArmorMaterial();
     public static final ArmorMaterial platedNetheriteArmorMaterial = new PlatedNetheriteArmorMaterial();
 
-    public static final Item GILDED_NETHERITE = new GildedNetheriteItem(new Item.Settings().group(GildedNetherite.GILDED_NETHERITE_GROUP).maxDamage(20));  // TODO: REMOVE DAMAGE
+    public static final Item GILDED_NETHERITE = new GildedNetheriteItem(new Item.Settings().group(GildedNetherite.GILDED_NETHERITE_GROUP));
     public static final Item GILDED_NETHERITE_HELMET = new ArmorItem(gildedNetheriteArmorMaterial, EquipmentSlot.HEAD, new Item.Settings().group(GildedNetherite.GILDED_NETHERITE_GROUP).fireproof());
     public static final Item GILDED_NETHERITE_CHESTPLATE = new ArmorItem(gildedNetheriteArmorMaterial, EquipmentSlot.CHEST, new Item.Settings().group(GildedNetherite.GILDED_NETHERITE_GROUP).fireproof());
     public static final Item GILDED_NETHERITE_LEGGINGS = new ArmorItem(gildedNetheriteArmorMaterial, EquipmentSlot.LEGS, new Item.Settings().group(GildedNetherite.GILDED_NETHERITE_GROUP).fireproof());
