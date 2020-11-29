16ade727ac8ac4f3eddc9d6627afaa9e7e0fb4ea
Gareth Coles <gdude2002@gmail.com>
2020-11-29 21:16:01 +0000

    Temporarily make gilded netherite enchantable

--- src/main/java/com/cumulusmc/gildednetherite/items/GildedNetheriteItem.java
+++ src/main/java/com/cumulusmc/gildednetherite/items/GildedNetheriteItem.java
@@ -1,9 +1,22 @@
 package com.cumulusmc.gildednetherite.items;
 
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 
 public class GildedNetheriteItem extends Item {
     public GildedNetheriteItem(Settings settings) {
         super(settings);
+    }
+
+    // TODO: REMOVE
+    @Override
+    public boolean isEnchantable(ItemStack stack) {
+        return true;
+    }
+
+    // TODO: REMOVE
+    @Override
+    public int getEnchantability() {
+        return 3;
     }
 }
--- src/main/java/com/cumulusmc/gildednetherite/items/RegisterItems.java
+++ src/main/java/com/cumulusmc/gildednetherite/items/RegisterItems.java
@@ -3,6 +3,7 @@ package com.cumulusmc.gildednetherite.items;
 import com.cumulusmc.gildednetherite.GildedNetherite;
 import com.cumulusmc.gildednetherite.materials.GildedNetheriteArmorMaterial;
 import com.cumulusmc.gildednetherite.materials.PlatedNetheriteArmorMaterial;
+import net.minecraft.enchantment.EnchantmentTarget;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.item.ArmorItem;
 import net.minecraft.item.ArmorMaterial;
@@ -16,7 +17,7 @@ public class RegisterItems {
     public static final ArmorMaterial gildedNetheriteArmorMaterial = new GildedNetheriteArmorMaterial();
     public static final ArmorMaterial platedNetheriteArmorMaterial = new PlatedNetheriteArmorMaterial();
 
-    public static final Item GILDED_NETHERITE = new GildedNetheriteItem(new Item.Settings().group(GildedNetherite.GILDED_NETHERITE_GROUP));
+    public static final Item GILDED_NETHERITE = new GildedNetheriteItem(new Item.Settings().group(GildedNetherite.GILDED_NETHERITE_GROUP).maxDamage(20));  // TODO: REMOVE DAMAGE
     public static final Item GILDED_NETHERITE_HELMET = new ArmorItem(gildedNetheriteArmorMaterial, EquipmentSlot.HEAD, new Item.Settings().group(GildedNetherite.GILDED_NETHERITE_GROUP).fireproof());
     public static final Item GILDED_NETHERITE_CHESTPLATE = new ArmorItem(gildedNetheriteArmorMaterial, EquipmentSlot.CHEST, new Item.Settings().group(GildedNetherite.GILDED_NETHERITE_GROUP).fireproof());
     public static final Item GILDED_NETHERITE_LEGGINGS = new ArmorItem(gildedNetheriteArmorMaterial, EquipmentSlot.LEGS, new Item.Settings().group(GildedNetherite.GILDED_NETHERITE_GROUP).fireproof());
